470e2d46bd7fe46b08187dee4a09bb53a5243ad9
Jürgen Gross <jngross95@gmail.com>
2026-01-01 20:59:11 +0100

    cc

--- Schule2/MyNat.lean
+++ Schule2/MyNat.lean
@@ -93,5 +93,58 @@ instance : Std.Commutative (α := MyNat) (· + ·) where
   comm := add_comm
 
 
-example (a b c : MyNat) : a + (b + c) = (a + b) + c := by
+example (a b c : MyNat) : a + (b + c) = (c + b) + a := by
   ac_rfl
+
+def mul : MyNat → MyNat → MyNat
+  | _, 0 => 0
+  | m, succ n => add m (mul m n)
+
+instance : HMul MyNat MyNat MyNat where
+  hMul := mul
+
+theorem mul_zero (m : MyNat) : m * 0 = 0 := by
+  rfl
+
+theorem mul_succ (m n : MyNat) : m * succ n = m + m * n := by
+  rfl
+
+theorem add_succ2 (m n : MyNat) :  succ n + m = n + succ m := by
+  calc succ n + m = succ (n + m) := by rw [succ_add]
+                _ = n + succ m := by rw [add_succ]
+
+
+
+
+theorem succ_mul (m n : MyNat) : (succ n) * m = n * m + m := by
+  apply mynat_induction (P := fun m => (succ n) * m = n * m + m)
+  · rfl
+  · intro m ia
+    calc (succ n) * (succ m) = (succ n) + (succ n) * m := by rw [mul_succ]
+                           _ = (succ n) + (n * m + m) := by rw [ia]
+                           _ = (succ n) + n * m + m := by ac_rfl
+                           _ = ((succ n) + m) + n*m := by ac_rfl
+                           _ = n + (succ m) + n*m := by rw [add_succ2]
+                           _ = n + n*m + (succ m) := by ac_rfl
+                           _ = n * (succ m) + (succ m) := by rw [mul_succ]
+
+
+
+
+theorem zero_mul (m : MyNat) : 0 * m = 0 := by
+  apply mynat_induction (P := fun m => 0 * m = 0)
+  · rfl
+  · intro m ia
+    rw [mul_succ]
+    rw [ia]
+    rw [add_zero]
+
+theorem mul_comm (m n : MyNat) : m * n = n * m := by
+  apply mynat_induction (P := fun n => m * n = n * m)
+  · rw [mul_zero m]
+    rw [zero_mul m]
+  · intro d ih
+    rw [mul_succ]
+    rw [ih]
+    rw [add_comm]
+    rw [succ_mul]
